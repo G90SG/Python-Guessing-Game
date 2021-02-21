3be434e49a9cddb6c6be47b407d19557de03109d
G90SG <71718345+G90SG@users.noreply.github.com>
2021-02-21 13:41:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-
+# Import Random Module
 import random
-
+# Create YesList and Nolist for def main()
 yesList = ["Yes", "Y", "y", "Yeah", "yes"]
 noList = ["No", "N", "n", "Nah", "no"]
-
+# Select a random number between 1 and 10 and assign to Number Variable
 number = random.randint(1,10)
 
-
+# Obtain input from the user, asking their name
 name = input (("Hey there, what's your name? "))
+# Concatentate in a sentence, asking them to choose a number between 1-10
 print ("Nice to meet you " + name + " please pick a number between 1 and 10. ")
-
+# Creating a function to house the IF statement checking the guess against the Number Variable, and printing a relevant statement
 def check_guess(guess):
   if guess == number:
     print ("Good Job " + name + ", you must be a mind reader.")
@@ -53,7 +54,7 @@ def check_guess(guess):
     print ("You couldn't be further from the right answer if you tried.")
   else: 
     print ("We both know that's not what I'm looking for here.")
-      
+# Create a function to hold the number of guesses remaining       
 def make_guesses():
   i=0
   while i < 6:
@@ -65,7 +66,7 @@ def make_guesses():
     check_guess(guess)
     i = i + 1 
     print("You have " + str(6-i)+ " tries remaining.")
-
+# Create Main function to hold whether the user would like to try again or not after an incorrect guess
 def main():
   make_guesses()
   restart = input ("Would you like to try again? ") 
@@ -77,4 +78,3 @@ def main():
     print ("It's like that is it? Good Bye then, " +name + ".")
     
 main()
-
